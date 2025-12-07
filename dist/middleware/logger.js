5dafd859cd29b25950a28e0aaafafe842249f220
Md. Mashrur Kabir <113287768+Mashrur-Kabir@users.noreply.github.com>
2025-12-07 22:35:52 +0600

    Delete dist directory

--- dist/middleware/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logger = (req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
-    next();
-};
-exports.default = logger;
